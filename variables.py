c96ee1b4617a9c96534ba24d6020a74539d8c461
YBoy <antonparfonov@gmail.com>
2022-06-28 12:04:21 +0300

    first tries

---
+++ variables.py
@@ -0,0 +1,10 @@
+myint = 1234
+print(myint)		# my int
+myfloat = 7.0
+print(myfloat)		# my float using .0
+myfloat = float(15)
+print (myfloat)		# my float using float()
+mystring = 'hello'
+print (mystring)	# my string using single quote
+mystring = "hello"
+print (mystring)	# my string using double quotes
